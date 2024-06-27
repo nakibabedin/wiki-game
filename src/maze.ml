42dbb31bba9488acdccf2b893e66c35510eb456f
nakibabedin@gmail.com <ubuntu@ip-172-31-16-197.us-east-2.compute.internal>
2024-06-27 20:38:58 +0000

    finish exercises  up to Djikstras

--- src/maze.ml
+++ src/maze.ml
@@ -1,16 +1,204 @@
 open! Core
 
+type direction =
+  | Left
+  | Right
+  | Up
+  | Down
+
+type pointType =
+  | Start
+  | Path
+  | Wall
+  | End
+[@@deriving equal]
+
+module Point = struct
+  type t =
+    { row : int
+    ; col : int
+    ; value : pointType
+    ; mutable visited : bool
+    }
+
+  let create row_val col_val point_type =
+    { row = row_val; col = col_val; value = point_type; visited = false }
+  ;;
+end
+
+type current_point =
+  { mutable row : int
+  ; mutable col : int
+  }
+
+let generate_list_of_points input_file =
+  let maze_list = In_channel.read_lines (File_path.to_string input_file) in
+  let maze_grid = List.map maze_list ~f:(fun line -> String.to_list line) in
+  let curr = { row = 0; col = 0 } in
+  let parsed_graph =
+    List.concat_map maze_grid ~f:(fun maze_line ->
+      curr.col <- 0;
+      let parsed_line =
+        List.map maze_line ~f:(fun maze_point ->
+          curr.col <- curr.col + 1;
+          match maze_point with
+          | 'S' -> Point.create curr.row (curr.col - 1) Start
+          | 'E' -> Point.create curr.row (curr.col - 1) End
+          | '.' -> Point.create curr.row (curr.col - 1) Path
+          | _ -> Point.create curr.row (curr.col - 1) Wall)
+      in
+      curr.row <- curr.row + 1;
+      parsed_line)
+  in
+  parsed_graph
+;;
+
+let start_point (list_of_points : Point.t list) =
+  let result =
+    List.filter list_of_points ~f:(fun point ->
+      match point.value with Start -> true | _ -> false)
+  in
+  match result with
+  | [] -> failwith "No Starting Point Detected"
+  | h :: _ -> h
+;;
+
+let is_valid_point (input_point : Point.t) (list_of_points : Point.t list) =
+  let point_in_list =
+    List.filter list_of_points ~f:(fun list_point ->
+      equal list_point.row input_point.row
+      && equal list_point.col input_point.col)
+  in
+  match point_in_list with
+  | [] -> failwith "point doesn't exist, this shouldn't happen"
+  | h :: _ -> (match h.value with Wall -> false | _ -> not h.visited)
+;;
+
+(* let is_end_point (input_point : Point.t) (list_of_points : Point.t list) =
+   let point_in_list = List.filter list_of_points ~f:(fun list_point -> equal
+   list_point.row input_point.row && equal list_point.col input_point.col) in
+   match point_in_list with | [] -> failwith "point doesn't exist, this
+   shouldn't happen" | h :: _ -> (match h.value with End -> true | _ ->
+   false) ;; *)
+
+(* From a point, move in a certain direction *)
+(* return an option with Some point or None if out of bounds *)
+let move_direction
+  (input_point : Point.t)
+  (direction : direction)
+  (list_of_points : Point.t list)
+  =
+  match direction with
+  | Left ->
+    let point =
+      List.filter list_of_points ~f:(fun list_point ->
+        equal list_point.row (input_point.row - 1)
+        && equal list_point.col input_point.col)
+    in
+    (match point with
+     | [] -> None
+     | h :: _ ->
+       (match is_valid_point h list_of_points with
+        | false -> None
+        | true -> Some h))
+  | Right ->
+    let point =
+      List.filter list_of_points ~f:(fun list_point ->
+        equal list_point.row (input_point.row + 1)
+        && equal list_point.col input_point.col)
+    in
+    (match point with
+     | [] -> None
+     | h :: _ ->
+       (match is_valid_point h list_of_points with
+        | false -> None
+        | true -> Some h))
+  | Up ->
+    let point =
+      List.filter list_of_points ~f:(fun list_point ->
+        equal list_point.row input_point.row
+        && equal list_point.col (input_point.col - 1))
+    in
+    (match point with
+     | [] -> None
+     | h :: _ ->
+       (match is_valid_point h list_of_points with
+        | false -> None
+        | true -> Some h))
+  | Down ->
+    let point =
+      List.filter list_of_points ~f:(fun list_point ->
+        equal list_point.row input_point.row
+        && equal list_point.col (input_point.col + 1))
+    in
+    (match point with
+     | [] -> None
+     | h :: _ ->
+       (match is_valid_point h list_of_points with
+        | false -> None
+        | true -> Some h))
+;;
+
+let get_move_direction (source : Point.t) (dest : Point.t) =
+  match dest.row - source.row, dest.col - source.col with
+  | 0, -1 -> Left
+  | 0, 1 -> Right
+  | 1, 0 -> Down
+  | -1, 0 -> Up
+  | _ -> failwith "Not a valid move, this shouldnt happen"
+;;
+
+let rec pathfinder (node : Point.t) (path : direction list) graph =
+  node.visited <- true;
+  match node.value with
+  | Wall -> failwith "Ran into a wall, this shouldn't happen"
+  | End -> Some path
+  | Path | Start ->
+    let directions = [ Left; Right; Up; Down ] in
+    let valid_next_moves_opt =
+      List.map directions ~f:(fun direction ->
+        move_direction node direction graph)
+    in
+    let valid_next_moves = List.filter_opt valid_next_moves_opt in
+    (match valid_next_moves with
+     | [] -> None
+     | _ ->
+       List.find_map valid_next_moves ~f:(fun next_node ->
+         pathfinder
+           next_node
+           (List.append path [ get_move_direction node next_node ])
+           graph))
+;;
+
 let solve_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:"parse a file containing a maze and find a solution"
     [%map_open
       let input_file =
-        flag "input" (required File_path.arg_type) ~doc:"FILE a file containing a maze"
+        flag
+          "input"
+          (required File_path.arg_type)
+          ~doc:"FILE a file containing a maze"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let maze = generate_list_of_points input_file in
+        (* let starting_point = start_point maze in *)
+        let start = start_point maze in
+        let path_opt = pathfinder start [] maze in
+        match path_opt with
+        | None -> failwith "No path in maze"
+        | Some path ->
+          List.iter path ~f:(fun direction ->
+            match direction with
+            | Left -> print_endline "Left"
+            | Right -> print_endline "Right"
+            | Up -> print_endline "Up"
+            | Down -> print_endline "Down")]
 ;;
 
-let command = Command.group ~summary:"maze commands" [ "solve", solve_command ]
+(* ignore (input_file : File_path.t); failwith "TODO"] *)
+
+let command =
+  Command.group ~summary:"maze commands" [ "solve", solve_command ]
+;;
--- src/wiki_game.ml
+++ src/wiki_game.ml
@@ -1,5 +1,58 @@
 open! Core
 
+module Article = struct
+
+  type t = 
+  {
+    name:String.t; url:String.t 
+  } [@@deriving compare, sexp, hash, equal]
+
+  let create ~url = 
+    let url_parts = String.split url ~on:'/' in
+    let name_opt = List.last url_parts in
+    match name_opt with
+    | None -> failwith "No name detected"
+    | Some name ->  { name ; url}
+  
+    (* let to_string t = t.name; *)
+  
+
+end
+
+module Connection = struct
+  
+  module T = struct
+  (* Article -> List of connected articles *)
+    type t = (Article.t * Article.t) [@@deriving compare, sexp, hash, equal]
+  end
+
+  include Comparable.Make(T)
+  
+end
+
+
+module G = Graph.Imperative.Graph.Concrete (String)
+
+(* We extend our [Graph] structure with the [Dot] API so that we can easily
+   render constructed graphs. Documentation about this API can be found here:
+   https://github.com/backtracking/ocamlgraph/blob/master/src/dot.mli *)
+module Dot = Graph.Graphviz.Dot (struct
+    include G
+
+    (* These functions can be changed to tweak the appearance of the
+       generated graph. Check out the ocamlgraph graphviz API
+       (https://github.com/backtracking/ocamlgraph/blob/master/src/graphviz.mli)
+       for examples of what values can be set here. *)
+    let edge_attributes _ = [ `Dir `None ]
+    let default_edge_attributes _ = []
+    let get_subgraph _ = None
+    let vertex_attributes v = [ `Shape `Box; `Label v; `Fillcolor 1000 ]
+    let vertex_name v = sprintf !"\"%s\"" v
+    let default_vertex_attributes _ = []
+    let graph_attributes _ = []
+  end)
+
+
 let not_namespace link =
   let option = (Wikipedia_namespace.namespace link) in
   match option with
@@ -58,6 +111,11 @@ let%expect_test "get_linked_articles" =
 ;;
 
 
+let correct_url url (how_to_fetch :File_fetcher.How_to_fetch.t) =
+  match how_to_fetch with
+  | Local _ -> url
+  | Remote -> if not (String.is_prefix ~prefix:"https://" url) then "https://en.wikipedia.org" ^ url else url
+
 
 let print_links_command =
   let open Command.Let_syntax in
@@ -70,17 +128,59 @@ let print_links_command =
         List.iter (get_linked_articles contents) ~f:print_endline]
 ;;
 
+(* returns a list of all of the connections that our graph would have *)
+let rec visualize_helper max_depth ~(origin:String.t)  ~how_to_fetch =
+
+  (* Setup the Graph from the current node *)
+
+  (* let graph = G.create () in *)
+
+  let origin_contents = File_fetcher.fetch_exn how_to_fetch ~resource:origin in
+  let adjacent_nodes = get_linked_articles origin_contents in
+  let edges = List.map adjacent_nodes ~f:(
+    fun edge_node -> (
+      let article_edge = Article.create ~url:edge_node in
+      let article_origin = Article.create ~url:origin in
+      (article_origin, article_edge)
+    )
+  ) in
+
+  match max_depth > 0 with
+  | false -> edges
+  | true -> (
+      let subgraph_connection_lists = List.concat_map adjacent_nodes ~f:(
+        fun node -> 
+          (* graph starting from node *)
+           visualize_helper (max_depth-1) ~origin:node ~how_to_fetch
+      ) in
+      edges @ subgraph_connection_lists
+  )
+
+
 (* [visualize] should explore all linked articles up to a distance of [max_depth] away
    from the given [origin] article, and output the result as a DOT file. It should use the
    [how_to_fetch] argument along with [File_fetcher] to fetch the articles so that the
    implementation can be tested locally on the small dataset in the ../resources/wiki
    directory. *)
 let visualize ?(max_depth = 3) ~origin ~output_file ~how_to_fetch () : unit =
-  ignore (max_depth : int);
+
+  let list_of_connections = visualize_helper max_depth ~origin ~how_to_fetch in
+  let graph = G.create () in
+  List.iter list_of_connections ~f:( 
+    fun connection -> 
+      let src, dest = connection in
+      G.add_edge graph src.name dest.name
+    );
+
+  Dot.output_graph (Out_channel.create (File_path.to_string output_file)) graph;
+  printf !"Done! Wrote dot file to %{File_path}\n%!" output_file;
+
+
+  (* ignore (max_depth : int);
   ignore (origin : string);
   ignore (output_file : File_path.t);
   ignore (how_to_fetch : File_fetcher.How_to_fetch.t);
-  failwith "TODO"
+  failwith "TODO" *)
 ;;
 
 let visualize_command =
@@ -108,6 +208,7 @@ let visualize_command =
         printf !"Done! Wrote dot file to %{File_path}\n%!" output_file]
 ;;
 
+
 (* [find_path] should attempt to find a path between the origin article and the
    destination article via linked articles.
 
@@ -116,13 +217,153 @@ let visualize_command =
    the ../resources/wiki directory.
 
    [max_depth] is useful to limit the time the program spends exploring the graph. *)
-let find_path ?(max_depth = 3) ~origin ~destination ~how_to_fetch () =
-  ignore (max_depth : int);
+
+
+(* let rec find_path_helper ?(max_depth = 3) ~(origin:String.t) ~(destination:String.t) ~how_to_fetch ~visited ()=
+
+  print_endline origin;
+
+  let correct_origin = correct_url origin how_to_fetch in
+  
+  match List.exists visited ~f:(String.equal correct_origin) with
+  | true -> None
+  | false -> (
+
+  (* Check if we're at destination *)
+  match (String.equal correct_origin destination) with 
+  | true -> Some [destination]
+  | false -> (
+    (* We aren't at destination, now check if we can search any farther *)
+      match (equal max_depth 0) with
+      (* if true, then there is no path from here onwards *)
+      | true -> None
+      | false -> ( 
+        (* try to search further down *)
+        let origin_contents = File_fetcher.fetch_exn how_to_fetch ~resource:correct_origin in
+        let adjacent_nodes = get_linked_articles origin_contents in 
+        let rest_of_path_opt = List.find_map adjacent_nodes ~f:(
+          fun next_node -> find_path_helper ~max_depth:(max_depth-1) ~origin:next_node ~destination ~how_to_fetch ~visited:(List.append visited [correct_origin]) ()
+        ) in 
+
+        match rest_of_path_opt with 
+        | None -> None
+        | Some rest_of_path -> Some (List.append [correct_origin] rest_of_path)
+      )))
+
+
+  (* ignore (max_depth : int);
   ignore (origin : string);
   ignore (destination : string);
   ignore (how_to_fetch : File_fetcher.How_to_fetch.t);
-  failwith "TODO"
+  failwith "TODO" *)
+;; *)
+
+(* Given a queue and destination, returns true if destination is in queue*)
+let check_for_dest ~destination ~(level:(string * string list) Base.Queue.t) ~how_to_fetch= 
+  let level_list = Queue.to_list level in 
+  List.exists level_list ~f:(fun list_item ->
+    
+    let first_item, _ = list_item in 
+    (* printf "first_item: %s\n"first_item; *)
+    (* printf "destination: %s\n"destination; *)
+    (* print_endline " "; *)
+    String.equal (correct_url destination how_to_fetch) (correct_url first_item how_to_fetch) )
+
 ;;
+
+(* to help with level order traversal *)
+let rec find_path_helper ?(max_depth=3) ~(level:(string * string list) Base.Queue.t) ~(destination:String.t) ~how_to_fetch ~visited() =
+
+
+  let final_level = check_for_dest ~destination ~level ~how_to_fetch in
+  match final_level with 
+  | true -> Some ( 
+    let (dest_opt) = Queue.find level ~f:(
+      fun final_item -> 
+        let node, _ = final_item in 
+        String.equal node destination) in 
+
+        match dest_opt with 
+        | None -> failwith "path not found at all -- Shouldn't happen"
+        | Some dest -> snd dest
+        
+        )
+  | false -> (
+        match equal max_depth 0 with 
+        | true -> None
+        | false -> (
+
+
+    let next_level :(string * string list) Base.Queue.t = Queue.create () in
+    let next_visited = List.append visited (Queue.to_list level) in
+    Queue.iter level ~f:(
+      
+      fun level_node ->  
+        let curr_node, curr_path = level_node in
+        let correct_origin = correct_url curr_node how_to_fetch in 
+
+        (* print_endline correct_origin; *)
+        
+        let origin_contents = File_fetcher.fetch_exn how_to_fetch ~resource:correct_origin in
+
+        let adjacent_nodes = get_linked_articles origin_contents in 
+        List.iter adjacent_nodes ~f:(fun next_node -> Queue.enqueue next_level (correct_url next_node how_to_fetch , (curr_path @ [next_node]))););
+
+      let next_level_without_dupes = Queue.filter next_level ~f:(fun node -> not (List.exists visited ~f:(
+        fun item -> String.equal (fst item) (fst node)
+      ))) in
+    
+      find_path_helper ~max_depth:(max_depth-1) ~level:next_level_without_dupes ~destination ~how_to_fetch ~visited:next_visited) ())
+;
+
+
+
+;;
+
+
+(* let rec find_path_helper ?(max_depth=3) ~(origin:String.t) ~(destination:String.t) ~how_to_fetch ~visited ~path () =
+
+  let correct_origin = correct_url origin how_to_fetch in 
+
+  match List.exists visited ~f:(String.equal correct_origin) with
+  | true -> None
+  | false -> (
+    (* Check if we're at destination *)
+      match (String.equal correct_origin destination) with 
+    | true -> Some [destination]
+    | false -> (
+      (* try to search further down *)
+      let origin_contents = File_fetcher.fetch_exn how_to_fetch ~resource:correct_origin in
+      let adjacent_nodes = get_linked_articles origin_contents in 
+      let level_order_traversal = Queue.of_list adjacent_nodes in
+      match check_for_dest ~destination ~level:level_order_traversal with 
+      | true -> Some (path @ [destination])
+      | false -> 
+
+
+        )) *)
+
+
+;;
+
+
+let find_path ?(max_depth = 2) ~(origin:String.t) ~(destination:String.t) ~(how_to_fetch:File_fetcher.How_to_fetch.t) () =
+  let visited = [] in
+  let first_level =  Queue.create () in
+  let new_origin =  correct_url origin how_to_fetch in
+  let new_destination = correct_url destination how_to_fetch in
+  Queue.enqueue first_level ((new_origin), [new_origin]);
+
+  find_path_helper ~max_depth ~level:first_level ~destination:new_destination ~how_to_fetch ~visited ()
+  
+  (* match how_to_fetch with
+  | Local _ -> find_path_helper ~max_depth ~origin ~destination ~how_to_fetch ~visited ()
+  | Remote -> (
+    let new_origin = "https://en.wikipedia.org/" @ origin in
+    find_path_helper ~max_depth ~new_origin ~destination ~how_to_fetch ~visited ()
+    ) *)
+  
+  ;;
 
 let find_path_command =
   let open Command.Let_syntax in
@@ -141,7 +382,7 @@ let find_path_command =
       fun () ->
         match find_path ~max_depth ~origin ~destination ~how_to_fetch () with
         | None -> print_endline "No path found!"
-        | Some trace -> List.iter trace ~f:print_endline]
+        | Some trace -> List.iter trace ~f:(fun item -> print_endline (correct_url item how_to_fetch))]
 ;;
 
 let command =
